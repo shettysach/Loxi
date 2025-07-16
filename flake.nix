4caa2631617ad79d07aad3d0880c948caa514bf2
Sachith Shetty <shettysachith47@gmail.com>
2025-07-16 16:05:05 +0530

    updated flake

--- flake.nix
+++ flake.nix
@@ -6,25 +6,19 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
         devShells.default = pkgs.mkShell {
           name = "odin";
-          buildInputs = with pkgs; [
-            odin
-            ols
-            lldb
-          ];
+          nativeBuildInputs = [pkgs.ols];
+          buildInputs = [pkgs. odin];
         };
       }
     );
